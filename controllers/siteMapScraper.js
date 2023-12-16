2984ba6280b4f4c4ac35068eedf5162a5533bd25
Aman Raj Rana <amanrajrana.dev@gmail.com>
2023-12-16 22:32:07 +0530

    Add sitMapScraper.js

---
+++ controllers/siteMapScraper.js
@@ -0,0 +1,57 @@
+// This is sitemapScraper.js
+import siteMapper from "sitemapper";
+import { load } from "cheerio";
+import axios from "axios";
+
+// Extract text by removing some things, like HTML
+function extractText(htmlContent) {
+  const $ = load(htmlContent);
+  $("style").remove();
+  $("script").remove();
+  $("[style]").removeAttr("style");
+  return {
+    content: $("body").text(),
+    title: $("title").text(),
+  };
+}
+
+const fetchTextContent = async (url) => {
+  try {
+    const response = await axios.get(url);
+
+    // Extract all text content
+    const { content, title } = extractText(response.data);
+
+    return {
+      url,
+      title,
+      content,
+      contentLength: content.length,
+    };
+  } catch (error) {
+    return {};
+  }
+};
+
+// Main function that orchestrates calling other functions and writing files
+export const scrapeWebsiteViaSiteMap = async (req, res) => {
+  const { url } = req.body;
+
+  if (!url) {
+    return res.status(400).json({ url: "url is required" });
+  }
+
+  //* Check url is sitemap or note
+  if (!url.includes(".xml")) {
+    return res.status(400).json({ error: "URL is not a sitemap" });
+  }
+
+  const sitemap = new siteMapper({ url });
+  const { sites } = await sitemap.fetch();
+
+  // Fetch all the sites in parallel
+  const promises = sites.map((url) => fetchTextContent(url));
+  const contents = await Promise.all(promises);
+
+  return res.json(contents);
+};
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { handleWebpageScrape } from "./controllers/webpageScraper.js";
+import { scrapeWebsiteViaSiteMap } from "./controllers/siteMapScraper.js";
 
 const app = express();
 const port = 4002;
@@ -8,11 +9,8 @@ const port = 4002;
 app.use(express.json());
 app.use(cors());
 
-app.post("/api/scrape", handleWebpageScrape);
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.post("/api/scrape/plain-url", handleWebpageScrape);
+app.post("/api/scrape/sitemap-url", scrapeWebsiteViaSiteMap);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
